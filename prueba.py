87b6add3025fa78f624520ad10b673a6ed0ae1d8
Alejandro Gonzalez Ballesteros <alejandrobright@gmail.com>
2018-06-15 14:25:51 -0500

    descomente codigo

--- prueba.py
+++ prueba.py
@@ -1,47 +1,47 @@
-# months = [
-# 'January',
-# 'February',
-# 'March',
-# 'April',
-# 'May',
-# 'June',
-# 'July',
-# 'August',
-# 'September',
-# 'October',
-# 'November',
-# 'December'
-# ]
+months = [
+'January',
+'February',
+'March',
+'April',
+'May',
+'June',
+'July',
+'August',
+'September',
+'October',
+'November',
+'December'
+]
 
-# endings = ['st', 'nd', 'rd'] + 17 * ['th'] \
-# + ['st', 'nd', 'rd'] + 7 * ['th'] \
-# + ['st']
-# year = input('Year: ')
-# month = input('Month (1-12): ')
-# day = input('Day (1-31): ')
-# month_number = int(month)
-# day_number = int(day)
+endings = ['st', 'nd', 'rd'] + 17 * ['th'] \
++ ['st', 'nd', 'rd'] + 7 * ['th'] \
++ ['st']
+year = input('Year: ')
+month = input('Month (1-12): ')
+day = input('Day (1-31): ')
+month_number = int(month)
+day_number = int(day)
 
-# month_name = months[month_number-1]
-# ordinal = day + endings[day_number-1]
-# print(month_name + ' ' + ordinal + ', ' + year)
+month_name = months[month_number-1]
+ordinal = day + endings[day_number-1]
+print(month_name + ' ' + ordinal + ', ' + year)
 
-# def pedir_pizza():
-#     print("Pedir Pizza".upper())
+def pedir_pizza():
+    print("Pedir Pizza".upper())
 
-# pedir_pizza()
+pedir_pizza()
 
-# vocales = "aeiou"
+vocales = "aeiou"
 
-# for vocal in vocales:
-#     print(vocal.upper())
+for vocal in vocales:
+    print(vocal.upper())
 
-# try:
-#     numero_1 = int(input("Dame un numero "))
-#     numero_2 = int(input("Dame otro numero "))
-# except ValueError:
-#     print("Ese no es un numero")
-# else:
-#     total = numero_1 + numero_2
-#     print("La suma es: "+ str(total))
+try:
+    numero_1 = int(input("Dame un numero "))
+    numero_2 = int(input("Dame otro numero "))
+except ValueError:
+    print("Ese no es un numero")
+else:
+    total = numero_1 + numero_2
+    print("La suma es: "+ str(total))
 
