9f2f8458d02d8e58a21c7c04527d21d1a556cda4
Alejandro Gonzalez Ballesteros <alejandrobright@gmail.com>
2018-06-20 15:36:45 -0500

    expresiones regulares

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
-import player
+# import player
 
-sorcerer = player.Sorcerer(hit_points=40,mana=80)
-print("El sorcerer tiene: ")
-print(sorcerer.hit_points)
-print(sorcerer.mana)
-print(sorcerer.vocation)
-print(sorcerer.cast_spell())
-print(sorcerer.movement_speed)
+# sorcerer = player.Sorcerer(hit_points=40,mana=80)
+# print("El sorcerer tiene: ")
+# print(sorcerer.hit_points)
+# print(sorcerer.mana)
+# print(sorcerer.vocation)
+# print(sorcerer.cast_spell())
+# print(sorcerer.movement_speed)
 
-knight = player.Knight(hit_points=80,mana=20)
-print("El Knight tiene: ")
-print(knight.hit_points)
-print(knight.mana)
-print(knight.vocation)
-print(knight.cast_spell())
-print(knight.movement_speed)
+# knight = player.Knight(hit_points=80,mana=20)
+# print("El Knight tiene: ")
+# print(knight.hit_points)
+# print(knight.mana)
+# print(knight.vocation)
+# print(knight.cast_spell())
+# print(knight.movement_speed)
+
+import re
+
+archivo = open("sample.txt",encoding="utf-8")
+
+informacion = archivo.read()
+
+archivo.close()
+
+#print(informacion)
+
+#print(re.match(r"0123456789",informacion))
+
+#print(re.search(r"0123456789",informacion))
+
+print(re.search(r"\+\d-\(\d\d\d\)-\d\d\d-\d\d\d\d",informacion))
